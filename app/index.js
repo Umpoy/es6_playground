57e68adbab4b90c295462039f385308fd034b60a
Umpoy <iumpoy@yahoo.com>
2017-11-07 11:49:06 -0800

    Completed axios demo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,29 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import $ from 'jquery';
+import axios from 'axios';
+
+
+function getData(){
+	axios.get('http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json')
+	.then( (resp) => {
+		console.log('Resp from server', resp);
+		displayData(resp.data.feed.entry);
+	}).catch((error) => {
+		console.log('Error from axios request: ', error)
+	})
+}
+
+function displayData(movieArray){
+	const movieHTML = movieArray.map((movie, index) => {
+		console.log('Movie', movie);
+
+		const container = $('<div>')
+		const h1 = $(`<h1>${movie['im:name'].label}</h1>`);
+		const img = $(`<img src='${movie['im:image'][2].label}'>`)
+		container.append(h1, img);
+		return container;
+	});
+
+	$('#root').append(movieHTML);
+}
+
+getData();
